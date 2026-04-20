2980269c92bcc692374bbd3a748eee2e5b87c739
noaht155 <noah21501@gmail.com>
2026-04-20 14:23:48 -0400

    Created PIO project & started test sketch

---
+++ Stridesynch/test/blinkTest.cpp
@@ -0,0 +1,11 @@
+#include <esp32-hal-gpio.h>
+void setup() {
+    pinMode(2, OUTPUT); // Built in LED on most ESP32 devkits
+}
+
+void loop() {
+    digitalWrite(2, HIGH);
+    delay(500);
+    digitalWrite(2, LOW);
+    delay(500);
+}
