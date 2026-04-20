578418ad5f159b6a54f9956ae4337a255a143ff1
noaht155 <noah21501@gmail.com>
2026-04-20 16:39:42 -0400

    Testing branch for I2C coms

--- Stridesynch/src/main.cpp
+++ Stridesynch/src/main.cpp
@@ -1,11 +1,51 @@
 #include <Arduino.h>
-void setup() {
-    pinMode(2, OUTPUT); // Built in LED on most ESP32 devkits
+#include <Wire.h>
+
+#define SDA_PIN 21
+#define SCL_PIN 22
+#define TCA_ADDRESS 0x70
+
+void tcaSelect(uint8_t channel) {
+    Wire.beginTransmission(TCA_ADDRESS);
+    Wire.write(1 << channel);
+    Wire.endTransmission();
 }
 
-void loop() {
-    digitalWrite(2, HIGH);
-    delay(1000);
-    digitalWrite(2, LOW);
-    delay(1000);
+void tcaCloseAll() {
+    Wire.beginTransmission(TCA_ADDRESS);
+    Wire.write(0x00);    // Writing 0 closes all channels
+    Wire.endTransmission();
 }
+
+void i2cScan(String label) {
+    Serial.println("--- " + label + " ---");
+    for (uint8_t addr = 1; addr < 127; addr++) {
+        Wire.beginTransmission(addr);
+        uint8_t error = Wire.endTransmission();
+        if (error == 0) {
+            Serial.print("Device found at 0x");
+            Serial.println(addr, HEX);
+        }
+    }
+    Serial.println("Scan complete");
+}
+
+void setup() {
+    delay(1000);
+    Serial.begin(115200);
+    
+    pinMode(21, INPUT_PULLUP);
+    pinMode(22, INPUT_PULLUP);
+    Wire.begin(SDA_PIN, SCL_PIN);
+    Wire.setClock(100000);  // Slow down to standard mode
+
+    // Scan before selecting any channel
+    tcaCloseAll();
+    i2cScan("Before channel select");
+
+    // Select channel 0 and scan again
+    tcaSelect(0);
+    i2cScan("After channel 0 select");
+}
+
+void loop() {}
